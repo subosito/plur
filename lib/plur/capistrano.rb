77e6f87e4163618a9a0f1864dda855a7b249cc7c
Alif Rachmawadi <subosito@gmail.com>
2013-01-31 11:53:09 +0700

    precompile assets locally

--- lib/plur/capistrano.rb
+++ lib/plur/capistrano.rb
@@ -1,3 +1,4 @@
 require "plur/capistrano/foreman"
 require "plur/capistrano/upstart"
 require "plur/capistrano/figaro"
+require "plur/capistrano/local"
---
+++ lib/plur/capistrano/local.rb
@@ -0,0 +1,21 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :local do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :assets, roles: :web, except: { no_release: true } do
+      remote_path = "#{user}@#{domain}:#{shared_path}"
+
+      run_locally "bundle exec rake assets:precompile"
+      run_locally "rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{remote_path}"
+      run_locally "bundle exec rake assets:clean"
+    end
+  end
+
+  # TODO: allow to turn it off
+  namespace :deploy do
+    namespace :assets do
+      task :precompile, roles: :web, except: { no_release: true } do
+        local.assets
+      end
+    end
+  end
+end

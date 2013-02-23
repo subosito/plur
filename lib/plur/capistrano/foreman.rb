9c7201792763ed84eb8c0fc8ddecacfc9f8491ef
Alif Rachmawadi <subosito@gmail.com>
2013-02-23 16:02:25 +0700

    reduce sudo usage

--- lib/plur/capistrano/foreman.rb
+++ lib/plur/capistrano/foreman.rb
@@ -18,7 +18,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :export, roles: :app do
       bundle_cmd          = fetch(:bundle_cmd, "bundle")
       foreman_format      = fetch(:foreman_format, "upstart")
-      foreman_location    = fetch(:foreman_location, "/etc/init")
+      foreman_location    = fetch(:foreman_location, "/etc/init/app")
       foreman_procfile    = fetch(:foreman_procfile, "Procfile")
       foreman_port        = fetch(:foreman_port, 5000)
       foreman_root        = fetch(:foreman_root, current_path)
@@ -35,7 +35,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       args << "-u #{foreman_user}"
       args << "-l #{foreman_log}"
       args << "-c #{foreman_concurrency}" if foreman_concurrency
-      run "cd #{release_path} && #{sudo} #{bundle_cmd} exec foreman export #{args.join(' ')}"
+      run "cd #{release_path} && #{bundle_cmd} exec foreman export #{args.join(' ')}"
     end
   end
 
--- lib/plur/capistrano/upstart.rb
+++ lib/plur/capistrano/upstart.rb
@@ -1,18 +1,20 @@
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :upstart do
+    _cset(:service_name) { "app/#{application}" }
+
     desc "Start the application services"
     task :start, roles: :app do
-      run "#{sudo} service #{application} start"
+      run "#{sudo} service #{service_name} start"
     end
 
     desc "Stop the application services"
     task :stop, roles: :app do
-      run "#{sudo} service #{application} stop"
+      run "#{sudo} service #{service_name} stop"
     end
 
     desc "Restart the application services"
     task :restart, roles: :app do
-      run "#{sudo} service #{application} start || #{sudo} service #{application} restart"
+      run "#{sudo} service #{service_name} start || #{sudo} service #{service_name} restart"
     end
   end
 
